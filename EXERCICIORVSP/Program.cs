93ded145f79a1de60fa86e239354f37fb0346952
Usuário <Usurio@DESKTOP-AR0HMME>
2025-08-05 18:32:53 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCICIORVSP/Program.cs
@@ -0,0 +1,41 @@
+﻿string[] guestList = { "Rebeca", "Nadia", "Noor", "Jonte" };
+string[] rsvp = new string[10];
+RSVP("Rebecca", 1, "none", true);
+RSVP("Nadia", 2, "Nuts", true);
+RSVP("Linh", 2, "none", false);
+RSVP("Tony", 1, "Jackfruit", true);
+RSVP("Noor", 4, "none", false);
+RSVP("Jonte", 2, "Stone fruit", false);
+ShowRSVPs();
+
+void RSVP(string name, int partySize, string allergies, bool inviteOnly)
+{
+    if (inviteOnly)
+    {
+        bool found = false;
+        foreach (string guest in guestList)
+        {
+            if (guest.Equals(name))
+            {
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+        {
+            Console.WriteLine($"Sorry, {name} is not on the guest list");
+            return;
+        }
+    }
+    rsvp[count] = $"Name: {name}, \tParty Size: {partySize}, \tAllergies: {allergies}";
+    count++;
+}
+
+void ShowRSVPs()
+{
+    Console.WriteLine("RSVPs:");
+    for (int i = 0; i < count; i++)
+    {
+        Console.WriteLine(rsvp[i]);
+    }
+}
